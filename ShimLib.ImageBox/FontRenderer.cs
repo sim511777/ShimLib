d2e44c9f2c53ab6f447224d6667f1c064c1fcebf
sim511777 <sim51177@gmail.com>
2020-04-27 22:45:30 +0900

    ImageBuffer 클래스 추가

---
+++ ShimLib.ImageBox/FontRenderer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShimLib {
+    public class FontRenderer {
+        ImageBuffer[] fontBuffers;
+        static FontRenderer() {
+            ImageBuffer fontImage = new ImageBuffer(Properties.Resources.tom_thumb_new);
+        }
+        
+        public static void DrawString(IntPtr dispBuf, int bw, int bh, string text, int x, int y) {
+            byte[] bytes = Encoding.ASCII.GetBytes(text);
+        }
+    }
+}
