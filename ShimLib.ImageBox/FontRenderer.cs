e917b7265300fe840ebc146d86cf0237a1ac32c9
sim511777 <sim51177@gmail.com>
2020-05-02 23:34:20 +0900

    FontRenderer 폰트 이미지 나누지 않고 하나로 통합

--- ShimLib.ImageBox/FontRenderer.cs
+++ ShimLib.ImageBox/FontRenderer.cs
@@ -8,54 +8,44 @@ using System.Threading.Tasks;
 
 namespace ShimLib {
     public class FontRenderer {
-        private ImageBuffer[] fontBuffers;
-        public FontRenderer(Bitmap bmp, int fontBW, int fontBH) {
-            fontBuffers = new ImageBuffer[256];
-            using (ImageBuffer fontImage = new ImageBuffer(bmp)) {
-                for (int i = 0; i < 256; i++) {
-                    IntPtr buf = Marshal.AllocHGlobal(fontBW * fontBH);
-                    fontBuffers[i] = new ImageBuffer(buf, fontBW, fontBH, 1, true);
-                    if (i >= 48 && i < 58) {
-                        for (int y = 0; y < fontBH; y++) {
-                            IntPtr src = fontImage.buf + fontImage.bw * y + fontBW * (i-48);
-                            IntPtr dst = buf + fontBW * y;
-                            Util.Memcpy(dst, src, fontBW);
-                        }
-                    } else {
-                        Util.Memset(buf, 255, fontBW * fontBH);
-                    }
-                }
-            }
+        private ImageBuffer fontImage;
+        private int fontBW;
+        private int fontBH;
+        public FontRenderer(Bitmap bmp, int _fontBW, int _fontBH) {
+            fontImage = new ImageBuffer(bmp);
+            fontBW = _fontBW;
+            fontBH = _fontBH;
         }
         
         public void DrawString(string text, IntPtr dispBuf, int dispBW, int dispBH, int dx, int dy, Color color) {
             int icolor = color.ToArgb();
-            byte[] bytes = Encoding.ASCII.GetBytes(text);
             int x = dx;
             int y = dy;
-            foreach (var b in bytes) {
-                var fontBuffer = fontBuffers[b];
-                if (b == 0x0d) {
+            foreach (char ch in text) {
+                if (ch == '\r') {
                     continue;
                 }
-                if (b == 0x0a) {
+                if (ch == '\n') {
                     x = dx;
-                    y += fontBuffer.bh;
+                    y += fontBH;
                     continue;
                 }
-                DrawByte(fontBuffer, dispBuf, dispBW, dispBH, x, y, icolor);
-                x += fontBuffer.bw;
+                if (ch >= '0' && ch <= '9') {
+                    int fontX = ('9' - '0') * fontBW;
+                    DrawChar(fontX, dispBuf, dispBW, dispBH, x, y, icolor);
+                }
+                x += fontBW;
             }
         }
 
-        private unsafe static void DrawByte(ImageBuffer fbuf, IntPtr dispBuf, int dispBW, int dispBH, int dx, int dy, int icolor) {
-            if (dx < 0 || dy < 0 || dx + fbuf.bw >= dispBW || dy + fbuf.bh >= dispBH)
+        private unsafe void DrawChar(int fontX, IntPtr dispBuf, int dispBW, int dispBH, int dx, int dy, int icolor) {
+            if (dx < 0 || dy < 0 || dx + fontBW >= dispBW || dy + fontBH >= dispBH)
                 return;
 
-            for (int y = 0; y < fbuf.bh; y++) {
-                byte* src = (byte*)fbuf.buf + fbuf.bw * y;
+            for (int y = 0; y < fontBH; y++) {
+                byte* src = (byte*)fontImage.buf + fontImage.bw * y + fontX;
                 int* dst = (int*)dispBuf + dispBW * (dy + y) + dx;
-                for (int x = 0; x < fbuf.bw; x++, src++, dst++) {
+                for (int x = 0; x < fontBW; x++, src++, dst++) {
                     if (*src == 0) {
                         *dst = icolor;
                     }
